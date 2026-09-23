821d9704d7c6b15c72413205dbad62758f97b906
tiagow2 <tiago.bala02@gmail.com>
2026-09-22 21:42:22 -0300

    feat[SYS-2]: Arrumando o build da aplicacao

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -19,6 +19,31 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureCompileSdk: () -> Unit = {
+        val android = project.extensions.findByName("android")
+        if (android != null) {
+            try {
+                val method = android.javaClass.getMethod("compileSdkVersion", Int::class.javaPrimitiveType)
+                method.invoke(android, 36)
+            } catch (_: Exception) {
+                try {
+                    val method = android.javaClass.getMethod("setCompileSdkVersion", Int::class.javaPrimitiveType)
+                    method.invoke(android, 36)
+                } catch (_: Exception) {}
+            }
+        }
+    }
+
+    if (project.state.executed) {
+        configureCompileSdk()
+    } else {
+        project.afterEvaluate {
+            configureCompileSdk()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
